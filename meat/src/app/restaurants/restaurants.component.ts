df2897d21a29c7b4e3bb321465bb88512a055844
LeoMartinsBDS <34400625+LeoMartinsBDS@users.noreply.github.com>
2018-03-17 17:26:20 -0300

    Add files via upload

--- meat/src/app/restaurants/restaurants.component.ts
+++ meat/src/app/restaurants/restaurants.component.ts
@@ -2,11 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { Restaurant } from './restaurant/restaurant.model'
 import {RestaurantsServices} from "./restaurants.service";
 
+
 @Component({
   selector: 'mt-restaurants',
   templateUrl: './restaurants.component.html'
 })
 export class RestaurantsComponent implements OnInit {
+
+
 
   restaurants: Restaurant[]
 
